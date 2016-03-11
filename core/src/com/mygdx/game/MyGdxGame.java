2a87ad895d17753a7c33bab1c0dc332da881dcaf
Marten Tilgner <mtilgner@wuhan.imp.fu-berlin.de>
2016-03-11 10:53:05 +0100

    Steuerung 1

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -10,15 +10,29 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends ApplicationAdapter {
 	private Sprite wellen1;
 	private Sprite wellen2;
+	private Sprite swimmer;
 	private SpriteBatch batch;
 	//Graphics Updates -> Variables to update positions
 	private float wellen_x_pos;
 	
 	private float geschwindigkeit;
+	private float beschleunigung;
+	//swimmer variables
+	private int swimmer_position;
+	//swimmer constants
+	private float swimmer_width;
+	private float swimmer_offset;
+	// game variables
+	private int score;
+	private int level;
+	private int health;
 	
 	// shortcuts for graphics fields
 	private int width, height;
 	private float ppiX, ppiY;
+	
+	
+	private EventListener steuerung;
 	
 	@Override
 	public void create () {
@@ -35,19 +49,51 @@ public class MyGdxGame extends ApplicationAdapter {
 		wellen2.setSize(width, height);
 		wellen_x_pos = 0;
 		
+		//init Swimmer_Grafik
+		swimmer = new Sprite(new Texture("schwimmer_aufsicht.png"));
+		swimmer_offset = (width / 7) * 1/8;
+		swimmer_width = (width / 7) * 3/4;
+		
 		//init geschwindigkeit
 		geschwindigkeit = 1.0f;
+		beschleunigung = 0;
+		
+		//init swimmer_position
+		swimmer_position = 4;
+		
+		//init score
+		score = 0;
+		level = 1;
+		health = 5;
+		
+		//erstelle und registriere Steuerung
+		steuerung = new EventListener();
+		steuerung.setGame(this);
+		Gdx.input.setInputProcessor(steuerung);
 	}
-
-	@Override
+	public void changeSwimmerPosition(int change){
+		swimmer_position += change;	
+		if(swimmer_position < 1){
+			swimmer_position = 1;
+		}
+		if(swimmer_position > 7)
+		{
+			swimmer_position = 7;
+		}
+	}
+	
+	@Override 
 	public void render () {
 		Gdx.gl.glClearColor(0, 0.6f, 0.9f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(wellen1, 0, wellen_x_pos % height, width, height);
 		batch.draw(wellen2, 0, (wellen_x_pos % (height)) + height, width, height);
+		batch.draw(swimmer, width / 7 * (swimmer_position-1) + swimmer_offset, 0, swimmer_width, swimmer_width);
 		update_graphics();
+		update_variables();
 		batch.end();
+		
 	}
 	
 	//Helpermethods
@@ -60,5 +106,11 @@ public class MyGdxGame extends ApplicationAdapter {
 		height = Gdx.graphics.getHeight();
 		ppiX = Gdx.graphics.getPpiX();
 		ppiY = Gdx.graphics.getPpiY();
+	}
+	
+	private void update_variables() {
+		geschwindigkeit += beschleunigung;
+		score += 1;
+		level = (score/10);
 	}
 }
---
+++ core/src/com/mygdx/game/Obstacle.java
@@ -0,0 +1,46 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Obstacle {
+	private Sprite sprite;
+	private int type;
+	private float posx;
+	private float posy;
+	private boolean[] blocked;
+	
+	public Obstacle(Sprite s, int t, float px, float py){
+		sprite = s;
+		type = t;
+		posx = px;
+		posy = py;
+		
+		
+	}
+	
+	
+	public int getType(){
+		return type;
+	}
+	
+	public boolean blocks(int b){
+		if(b < 1 || b > 7){
+			// error
+		}
+		
+	}
+	
+	
+	public float getY(){
+		return posy;
+	}
+	
+	public void draw(SpriteBatch batch){
+		batch.draw(sprite, posx, posy);
+	}
+	
+	
+	
+
+}
