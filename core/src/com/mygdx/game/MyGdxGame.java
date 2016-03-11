9b337888adab7c82d077944a973f6877cfeba736
Daria Kravets <krawez@guangzhou.imp.fu-berlin.de>
2016-03-11 11:13:25 +0100

    bla

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,8 +2,11 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -11,6 +14,16 @@ public class MyGdxGame extends ApplicationAdapter {
 	private Sprite wellen1;
 	private Sprite wellen2;
 	private SpriteBatch batch;
+	
+	private Sprite herz_leer;
+	private Sprite herz_voll;
+	
+	// Schrift
+	private BitmapFont font;
+
+	
+	int health;
+	
 	//Graphics Updates -> Variables to update positions
 	private float wellen_x_pos;
 	
@@ -28,6 +41,18 @@ public class MyGdxGame extends ApplicationAdapter {
 		//New Sprite Batch
 		batch = new SpriteBatch();
 		
+		herz_leer = new Sprite(new Texture("herz_leer.png"));
+		herz_voll = new Sprite(new Texture ("herz_voll.png"));
+		
+		herz_voll.setSize(width/18, height/18);
+		herz_leer.setSize(width/18, height/18);
+		
+		health = 5;
+		
+		//Schrift
+		font = new BitmapFont(Gdx.files.internal("arial-15.fnt"), false);
+		font.setColor(Color.BLACK);
+		
 		//init Wellentextur
 		wellen1 = new Sprite(new Texture("wellen.png"));
 		wellen1.setSize(width, height);
@@ -44,15 +69,72 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0.6f, 0.9f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		
+		
 		batch.draw(wellen1, 0, wellen_x_pos % height, width, height);
 		batch.draw(wellen2, 0, (wellen_x_pos % (height)) + height, width, height);
 		update_graphics();
+		
+//		batch.draw(herz_leer, 5, 5, width/17, height/17 ); 
+			
 		batch.end();
+		
+		
+	
+		
 	}
 	
 	//Helpermethods
 	private void update_graphics(){
 		wellen_x_pos -= geschwindigkeit;
+		
+		// Herzen update
+		
+		if (health == 5){
+			batch.draw(herz_voll, 19, 440, width/18, height/18);
+			batch.draw(herz_voll, 55, 440, width/18, height/18);
+			batch.draw(herz_voll, 90, 440, width/18, height/18);
+			batch.draw(herz_voll, 125, 440, width/18, height/18);
+			batch.draw(herz_voll, 160, 440, width/18, height/18);
+		
+		} else if (health == 4) {
+			batch.draw(herz_voll, 19, 440, width/18, height/18);
+			batch.draw(herz_voll, 55, 440, width/18, height/18);
+			batch.draw(herz_voll, 90, 440, width/18, height/18);
+			batch.draw(herz_voll, 125, 440, width/18, height/18);
+			batch.draw(herz_leer, 160, 440, width/18, height/18);
+			
+		}else if (health == 3) {
+			batch.draw(herz_voll, 19, 440, width/18, height/18);
+			batch.draw(herz_voll, 55, 440, width/18, height/18);
+			batch.draw(herz_voll, 90, 440, width/18, height/18);
+			batch.draw(herz_leer, 125, 440, width/18, height/18);
+			batch.draw(herz_leer, 160, 440, width/18, height/18);
+		
+		
+		}else if (health == 2) {
+			batch.draw(herz_voll, 19, 440, width/18, height/18);
+			batch.draw(herz_voll, 55, 440, width/18, height/18);
+			batch.draw(herz_leer, 90, 440, width/18, height/18);
+			batch.draw(herz_leer, 125, 440, width/18, height/18);
+			batch.draw(herz_leer, 160, 440, width/18, height/18);
+		
+		}else if (health == 1) {
+			batch.draw(herz_voll, 19, 440, width/18, height/18);
+			batch.draw(herz_leer, 55, 440, width/18, height/18);
+			batch.draw(herz_leer, 90, 440, width/18, height/18);
+			batch.draw(herz_leer, 125, 440, width/18, height/18);
+			batch.draw(herz_leer, 160, 440, width/18, height/18);
+		
+		}
+		else if (health == 0) {
+			batch.draw(herz_leer, 19, 440, width/18, height/18);
+			batch.draw(herz_leer, 55, 440, width/18, height/18);
+			batch.draw(herz_leer, 90, 440, width/18, height/18);
+			batch.draw(herz_leer, 125, 440, width/18, height/18);
+			batch.draw(herz_leer, 160, 440, width/18, height/18);
+		
+		}
 	}
 	
 	private void readGraphics() {
@@ -61,4 +143,5 @@ public class MyGdxGame extends ApplicationAdapter {
 		ppiX = Gdx.graphics.getPpiX();
 		ppiY = Gdx.graphics.getPpiY();
 	}
+	
 }
