df8bb7e90f7b91e05ce97de68ab835f94460e1a1
Luise Schricker <luise.schricker@googlemail.com>
2016-03-23 16:37:14 +0100

    Taucherauftauchen gefixt

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1232,6 +1232,7 @@ public class MyGdxGame extends ApplicationAdapter {
 			state = GameState.LOWERWORLD;
 			body.setLinearVelocity(0, 0);
 			body.setTransform(0, 100, 0);
+			
 	
 			//Unterwasser-Hindernis initialisieren
 			//TODO: -> Dynamisch annpassen -> Obstacle ueber init_Obstacle_lowerworld-Methode erzeugen
@@ -1549,8 +1550,10 @@ public class MyGdxGame extends ApplicationAdapter {
 				}, 1000);
 				
 			}
-		}	
-			
+		}
+		
+		//auftauchen
+		if(hindernis_lowerworld_lower.getLaenge() <= (-10)) body.setLinearVelocity(0, 1000);
 		// GameOver check
 		if (health <= 0) {
 			setGameOver();
